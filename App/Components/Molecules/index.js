9154a99a8a54d8bbec5ded167ccc2489be8474c4
Abdul Rafeh <abdulrafeh857@gmail.com>
2024-05-21 19:13:28 +0500

    molecules comp new import

--- App/Components/Molecules/index.js
+++ App/Components/Molecules/index.js
@@ -1,1 +1,3 @@
-export { default as Button } from './Button'
+export { default as Button } from './Button';
+export { default as TreeView } from './TreeView';
+export { default as SelectedProducts } from './SelectedProducts';
