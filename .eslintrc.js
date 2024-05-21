3e2e08128e5ed8f6eff0b573bcbf93b2ce9a8d76
Abdul Rafeh <abdulrafeh857@gmail.com>
2024-05-21 11:35:47 +0500

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  rules: {
+    'react/no-unescaped-entities': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'comma-dangle': 'never'
+  }
+};
---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'none',
+  arrowParens: 'avoid',
+  semi: true,
+  endOfLine: 'auto',
+  rules: {
+    semi: [2, true]
+}
+}
